46ac4671d37a0a060a28286e8ace9d5a00c667aa
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 01:08:10 +0530

    basic routes added

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,35 @@
+import 'package:farm_setu_assignment/modules/home-screen/home_screen.dart';
+import 'package:farm_setu_assignment/modules/initial/initial_screen.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
+  //LOGIN
   static const String login = 'login';
+
   static const String register = 'register';
-  static const String forgotPassword = 'forgotPassword';
 
 //HOME
   static const String home = 'home';
 
-//
+  //INITIAL
+  static const String initial = 'initial';
+
+  static Route<dynamic>? getGeneratedRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case home:
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: home),
+          builder: (_) => const MyHomePage(),
+        );
+
+      case initial:
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: initial),
+          builder: (_) => const InitScreen(),
+        );
+
+      default:
+        return null;
+    }
+  }
 }
