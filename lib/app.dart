59f548bf953e5cadf3130b298fee0bce25cedc04
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 01:07:51 +0530

    materialapp -> getmaterial app

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,27 @@
-import 'package:farm_setu_assignment/modules/home-screen/home-screen.dart';
+import 'package:farm_setu_assignment/configs/app-config.dart';
+import 'package:farm_setu_assignment/routes.dart';
+import 'package:farm_setu_assignment/shared/services/navigation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    //GET MATERIAL APP FOR SNACKBAR ETC USE
+    return GetMaterialApp(
+      navigatorKey: appNavigationService.myNavigatorKey,
+      title: appConfig.appName ?? "FARM SETU",
+      debugShowCheckedModeBanner: appConfig.showDebugBanner,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      // themeMode: themeController.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      // darkTheme: themeController.themeDark,
+      initialRoute: Routes.initial,
+      onGenerateRoute: Routes.getGeneratedRoute,
     );
   }
 }
