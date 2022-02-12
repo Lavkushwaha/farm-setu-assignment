7e168d7917b53b0b4955e4375508699d5dcbbf08
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 01:17:29 +0530

    resp sent back

--- lib/shared/api_providers/weather_api_provider.dart
+++ lib/shared/api_providers/weather_api_provider.dart
@@ -1,9 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:farm_setu_assignment/shared/models/api_result_model.dart';
-import 'package:farm_setu_assignment/shared/models/weather_model.dart';
 import 'package:farm_setu_assignment/shared/services/api_service.dart';
 import 'package:farm_setu_assignment/utils/constants.dart';
-import 'package:flutter/material.dart';
 
 class WeatherAPIProvider {
   DioClient? dioClient;
@@ -19,12 +17,10 @@ class WeatherAPIProvider {
       final response = await dioClient?.get("movie/popular",
           queryParameters: {"appid": API_KEY, "lat": lat, "lon": lon});
 
-      // debugPrint("Response: ${response}");
+      // Weather w = Weather.fromJson(response);
+      // debugPrint(w.name);
 
-      Weather w = weatherFromJson(response);
-      debugPrint(w.name);
-
-      return ApiResult.success(weatherToJson(response));
+      return ApiResult.success(response);
     } catch (e) {
       return ApiResult.error(e);
     }
