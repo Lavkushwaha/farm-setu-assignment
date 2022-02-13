34ccd8ed92ceae40f0ab08156d7630332ca12f4c
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 13:53:13 +0530

    base url updated

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 library constants;
 
-const String BASEURL = 'https://api.openweathermap.org/data/2.5/weather?';
+const String BASEURL = 'https://api.openweathermap.org/data/2.5/';
 
 const String API_KEY = '13ce3086cd64c9643fbf4cd5e304c1d9';
