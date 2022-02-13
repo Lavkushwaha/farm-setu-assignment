5ec0979710dd1aebc36151c107ee984cf3b6fe64
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 13:53:01 +0530

    intial bloc connection condition added

--- lib/blocs/initial_bloc/initial_bloc.dart
+++ lib/blocs/initial_bloc/initial_bloc.dart
@@ -1,12 +1,16 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:farm_setu_assignment/blocs/base_bloc.dart';
 import 'package:farm_setu_assignment/blocs/weather_bloc/weather_bloc.dart';
 import 'package:farm_setu_assignment/routes.dart';
+import 'package:farm_setu_assignment/shared/services/connection_service.dart';
 import 'package:farm_setu_assignment/shared/services/navigation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
 class InitialBloc extends BaseBloc {
   BehaviorSubject<bool> loadingController = BehaviorSubject<bool>.seeded(true);
+  // late bool _hasNetworkConnection;
+  // late bool _fallbackViewOn;
 
   getCurrentLocation() {
     loadingController.add(true);
@@ -15,14 +19,20 @@ class InitialBloc extends BaseBloc {
   }
 
   checkForData() async {
-    loadingController.add(true);
-    debugPrint("checking for data");
+    if (connectionStatus.connectivity.checkConnectivity() !=
+        ConnectivityResult.none) {
+      loadingController.add(true);
+      debugPrint("checking for data");
 
-    await weatherBloc.getWeatherData("72", "23");
+      await weatherBloc.getWeatherData();
 
-    loadingController.add(false);
+      // loadingController.add(false);
+      appNavigationService.pushReplacementNamed(Routes.home);
+    } else {
+      debugPrint("No internet connection");
 
-    appNavigationService.pushReplacementNamed(Routes.home);
+      loadingController.add(false);
+    }
   }
 
   @override
