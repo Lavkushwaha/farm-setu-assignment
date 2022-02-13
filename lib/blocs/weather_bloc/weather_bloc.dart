8bae4d9e3f30c66f8959dcaf995b1da5cf752bef
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 13:54:16 +0530

    weather bloc wip

--- lib/blocs/weather_bloc/weather_bloc.dart
+++ lib/blocs/weather_bloc/weather_bloc.dart
@@ -2,7 +2,9 @@ import 'package:farm_setu_assignment/blocs/base_bloc.dart';
 import 'package:farm_setu_assignment/shared/models/api_result_model.dart';
 import 'package:farm_setu_assignment/shared/models/weather_model.dart';
 import 'package:farm_setu_assignment/shared/repositories/weather_repository.dart';
+import 'package:farm_setu_assignment/shared/services/geolocation_service.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:rxdart/rxdart.dart';
 
 class WeatherBloc extends BaseBloc {
@@ -10,18 +12,65 @@ class WeatherBloc extends BaseBloc {
 
   Weather? latestWeather;
 
-  getWeatherData(lat, lon) async {
+  getWeatherData() async {
     loadingController.add(true);
 
     try {
-      ApiResult<dynamic> state =
-          await weatherRepository.getWeatherData('72', '23');
+      Position loc = await geoLocationService.getCurrentPosition();
+      ApiResult<dynamic> state = await weatherRepository.getWeatherData(
+          loc.latitude.toString(), loc.longitude.toString());
       if (state is SuccessState) {
-        //return data to main isolate
-
         debugPrint('weather data fetched');
 
         latestWeather = Weather.fromJson(state.value);
+
+        loadingController.sink.add(false);
+        return state.value;
+      } else if (state is ErrorState) {
+        loadingController.sink.add(false);
+        return null;
+      }
+    } catch (e) {
+      loadingController.addError(e);
+      loadingController.sink.add(false);
+    }
+  }
+
+  getDailyWeather() async {
+    loadingController.add(true);
+
+    try {
+      Position loc = await geoLocationService.getCurrentPosition();
+      ApiResult<dynamic> state = await weatherRepository.getWeatherDaily(
+          loc.latitude.toString(), loc.longitude.toString());
+      if (state is SuccessState) {
+        debugPrint('weather daily fetched');
+
+        // latestWeather = Weather.fromJson(state.value);
+
+        loadingController.sink.add(false);
+        return state.value;
+      } else if (state is ErrorState) {
+        loadingController.sink.add(false);
+        return null;
+      }
+    } catch (e) {
+      loadingController.addError(e);
+      loadingController.sink.add(false);
+    }
+  }
+
+  getWeatherhistory() async {
+    loadingController.add(true);
+
+    try {
+      Position loc = await geoLocationService.getCurrentPosition();
+      ApiResult<dynamic> state = await weatherRepository.getWeatherDaily(
+          loc.latitude.toString(), loc.longitude.toString());
+      if (state is SuccessState) {
+        debugPrint('weather daily fetched');
+
+        // latestWeather = Weather.fromJson(state.value);
 
         loadingController.sink.add(false);
         return state.value;
