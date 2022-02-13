5ab471059f8fd1a112b11641ca02b00e467186f4
Lavkant Kachhwaha <kush373@gmail.com>
2022-02-13 13:52:45 +0530

    connection initialized

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'dart:async';
 
 import 'package:farm_setu_assignment/app.dart';
 import 'package:farm_setu_assignment/configs/app-config.dart';
+import 'package:farm_setu_assignment/shared/services/connection_service.dart';
+import 'package:farm_setu_assignment/shared/services/geolocation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   appConfig.initalize();
+
+  connectionStatus.initialize();
+  connectionStatus.checkConnection();
+  await geoLocationService.handlePermission();
 
   FlutterError.onError = (FlutterErrorDetails details) {
     debugPrint("FLUTTER CONTEXT : ${details.context}");
